c6430b37d74d6283ab86ad0e00bd9ef4d24ab833
Saulius Grigaliunas <saulius@ninja.lt>
2012-05-15 14:03:56 +0100

    tire configuration for Commodity

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,4 +1,13 @@
 class Commodity < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
+
+  tire do
+    mapping do
+      indexes :id,           index: :not_analyzed
+      indexes :chapter_id,   index: :not_analyzed # not sure this will be needed yet
+      indexes :code,         index: :not_analyzed
+      indexes :description,  analyzer: 'snowball'
+    end
+  end
 end
