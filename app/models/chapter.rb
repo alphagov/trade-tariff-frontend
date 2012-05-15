69c22cbd15a75f54af727b8d1abaf8acdc2f51c5
Saulius Grigaliunas <saulius@ninja.lt>
2012-05-15 13:10:22 +0100

    add tire config for chapters

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,13 @@
 class Chapter < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
+
+  tire do
+    mapping do
+      indexes :id,           index: :not_analyzed
+      indexes :section_id,   index: :not_analyzed # not sure this will be needed yet
+      indexes :number
+      indexes :description,  analyzer: 'snowball'
+    end
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -6,6 +6,7 @@ class Section < ActiveRecord::Base
     mapping do
       indexes :id,           index: :not_analyzed
       indexes :title,        analyzer: 'snowball'
+      indexes :position # in case we need to sort
     end
   end
 end
