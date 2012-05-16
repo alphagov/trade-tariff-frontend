20fdd65df9f0225100b0ec0b850edcb7c56cd049
Saulius Grigaliunas <saulius@ninja.lt>
2012-05-16 14:43:14 +0100

    should run spec on test runs

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,6 @@
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 RAILS_ENV=test bundle exec rake db:setup
 RAILS_ENV=test bundle exec rake db:migrate
-RAILS_ENV=test bundle exec rake ci:setup:rspec
+RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
