ba25cd2b6c24cea95a6c2087579c4949c43bf9f5
Saulius Grigaliunas <saulius@ninja.lt>
2012-05-30 15:15:08 +0100

    Headings can have measures too. Display them if they are present.

--- app/models/heading.rb
+++ app/models/heading.rb
@@ -3,14 +3,21 @@ require 'api_entity'
 class Heading
   include ApiEntity
 
-  attr_accessor :id, :description, :code, :commodities, :short_code
+  attr_accessor :id, :description, :code, :commodities, :short_code,
+                :import_measures, :export_measures
 
   has_one :chapter
   has_one :section
   has_many :commodities
+  has_many :import_measures, class_name: 'Measure'
+  has_many :export_measures, class_name: 'Measure'
 
   def self.find(id)
     new(get("/headings/#{id}"))
+  end
+
+  def has_measures?
+    import_measures.present? || export_measures.present?
   end
 
   def to_param
