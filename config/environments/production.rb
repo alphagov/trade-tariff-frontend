c0d0b66ed3325e69022c4a48b669ef5127d7cf90
James Abley <james.abley@gmail.com>
2012-05-31 13:59:34 +0100

    Note that we need to fix it properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,5 +69,6 @@ Uktt::Application.configure do
   config.slimmer.asset_host = Plek.current.find('assets')
 
   # Host for Trade Tariff API endpoint
+  # TODO: externalize this lookup via Plek?
   config.api_host = "https://tariff-api.preview.alphagov.co.uk"
 end
