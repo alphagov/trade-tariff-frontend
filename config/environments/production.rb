bba6d6d6ddac1af0e76c38d6e4cfc1e0a54c20b3
James Abley <james.abley@gmail.com>
2012-06-01 19:36:48 +0100

    Revert "use tariff as asset host"
    This reverts commit a33cec8abd4642a03567c42d90ae551268744a92.
    
    I suspect it causes a circular proxy thing? Site is not working
    currently, so backing out this change.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Uktt::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_controller.asset_host = Plek.current.find('tariff')
-  config.slimmer.asset_host = Plek.current.find('tariff')
+  config.action_controller.asset_host = Plek.current.find('assets')
+  config.slimmer.asset_host = Plek.current.find('assets')
 
   # Host for Trade Tariff API endpoint
   # TODO: externalize this lookup via Plek?
