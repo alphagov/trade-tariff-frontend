385221e158ba585b5ef586099918844fa0fcb13a
Matthew Ford <matt@bitzesty.com>
2012-06-07 11:22:54 +0100

    heading import export measures

--- app/controllers/headings_controller.rb
+++ app/controllers/headings_controller.rb
@@ -3,4 +3,16 @@ class HeadingsController < ApplicationController
     @heading = Heading.find(params[:id])
     @commodities = @heading.commodities
   end
+  def import_measures
+    @measurable = Heading.find(params[:id])
+    @import_measures = @measurable.import_measures
+    render "measures/import"
+  end
+
+  def export_measures
+    @measurable = Heading.find(params[:id])
+    @export_measures = @measurable.export_measures
+    render "measures/export"
+  end
+
 end
