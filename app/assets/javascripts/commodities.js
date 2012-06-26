9532f798d79ccc8269952e02f13bf7ff4114a155
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-06-26 09:47:07 -0700

    Add rudimentary open/close functionality

--- app/assets/javascripts/commodities.js
+++ app/assets/javascripts/commodities.js
@@ -1,62 +1,21 @@
-var loadExportMeasures, loadImportMeasures, loadMeasures, showPopovers;
-
-showPopovers = function() {
-  $("a[ref='popover']").each(function(){
-    $(this).popover();
-
-    $(this).click(function(e){
-      e.preventDefault();
+/*global $ */
+/*jslint
+ white: true */
+$(function() {
+    "use strict";
+    // hide all child lists
+    $('ul.commodities .has_children > ul').addClass('visuallyhidden');
+    // allow expansion based on clicking
+    $('li.has_children').on('click', function(e) {
+        e.stopPropagation();
+        var child = $('>ul', this);
+        if (child.is('.visuallyhidden')) {
+            child.removeClass('visuallyhidden');
+            $(this).addClass('open');
+        }
+        else {
+            child.addClass('visuallyhidden');
+            $(this).removeClass('open');
+        }
     });
-  });
-}
-
-loadMeasures = function() {
-  var e_objectId, e_type, i_objectId, i_type, measures;
-  measures = $("#measures-js").length !== 0;
-  if (measures) {
-    i_objectId = $("#import-measures").attr("data-id");
-    i_type = $("#import-measures").attr("data-class");
-    loadImportMeasures(i_objectId, i_type);
-    e_objectId = $("#export-measures").attr("data-id");
-    e_type = $("#export-measures").attr("data-class");
-    loadExportMeasures(e_objectId, e_type);
-
-    return true;
-  }
-};
-
-loadImportMeasures = function(oid, type) {
-  return $.ajax({
-    type: 'GET',
-    headers: {
-      'X-AJAX': true
-    },
-    url: "/" + type + "/" + oid + "/import_measures",
-    success: function(data) {
-      showPopovers();
-
-      return $("#import-measures").append(data);
-    },
-    error: function(data) {
-      return console.log(':(');
-    }
-  });
-};
-
-loadExportMeasures = function(oid, type) {
-  return $.ajax({
-    type: 'GET',
-    headers: {
-      'X-AJAX': true
-    },
-    url: "/" + type + "/" + oid + "/export_measures",
-    success: function(data) {
-      showPopovers();
-
-      return $("#export-measures").append(data);
-    },
-    error: function(data) {
-      return console.log(':(');
-    }
-  });
-};
+});
