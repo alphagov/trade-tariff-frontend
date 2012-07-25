c6083f4aa77a500b7dcb415f4fc1eb889e255707
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-25 16:11:00 +0100

    Display duty expression in measure conditions.

--- app/models/measure_condition.rb
+++ app/models/measure_condition.rb
@@ -3,5 +3,5 @@ require 'api_entity'
 class MeasureCondition
   include ApiEntity
 
-  attr_accessor :document_code, :action, :requirement, :condition
+  attr_accessor :document_code, :action, :requirement, :condition, :duty_expression
 end
