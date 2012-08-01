1a54407b6f76233c546319a703c7ec00204ff73b
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-01 11:49:58 +0300

    Add Heading nesting. Fix style for commodity code display.

--- app/models/heading.rb
+++ app/models/heading.rb
@@ -4,17 +4,19 @@ class Heading
   include ApiEntity
 
   attr_accessor :description, :code, :commodities, :short_code,
-                :import_measures, :export_measures,
+                :import_measures, :export_measures, :leaf,
                 :declarable, :third_country_duty_rate, :uk_vat_rate,
                 :synonyms
 
   has_one :chapter
   has_one :section
   has_many :commodities, class_name: 'Commodity'
+  has_many :children, class_name: 'Heading'
   has_many :import_measures, class_name: 'Measure'
   has_many :export_measures, class_name: 'Measure'
 
   alias :declarable? :declarable
+  alias :leaf? :leaf
 
   def commodity_code
     code.first(10)
