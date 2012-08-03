d9667a13ad76675df6648a22fbdf493804c91b74
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-03 10:58:36 +0300

    Add missing VCR cassettes. Add search case with exact match.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 
-describe SearchController, "POST to #search", vcr: { cassette_name: "search#search" }  do
-  let(:query) { "horses" }
+describe SearchController, "POST to #search" do
+  context "with exact match query", vcr: { cassette_name: "search#search_exact" }  do
+    let(:query) { "01" }
 
-  before(:each) do
-    get :search, { q: query }
+    before(:each) do
+      get :search, { q: query }
+    end
+
+    it { should respond_with(:redirect) }
+    it { should assign_to(:search) }
+    it 'assigns search attribute' do
+      assigns[:search].q.should == query
+    end
   end
 
-  it { should respond_with(:success) }
-  it { should assign_to(:search) }
-  it 'assigns search attribute' do
-    assigns[:search].q.should == query
+  context "with fuzzy match query", vcr: { cassette_name: "search#search_fuzzy" }  do
+    let(:query) { "horses" }
+
+    before(:each) do
+      get :search, { q: query }
+    end
+
+    it { should respond_with(:success) }
+    it { should assign_to(:search) }
+    it 'assigns search attribute' do
+      assigns[:search].q.should == query
+    end
   end
 end
