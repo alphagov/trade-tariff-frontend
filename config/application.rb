09f6082c5aee4d1ae2da3468a6f987834d524535
Matthew Ford <matt@bitzesty.com>
2012-08-08 13:39:26 +0100

    add trade-tariff as the asset prefix

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module TariffWeb
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = false #will move to mongo for API
+    config.assets.prefix = "trade-tariff"
 
     # Enable the asset pipeline
     config.assets.enabled = true
