f54e92bb312375df6dfc345f8cd8a7c6428525ff
Matthew Ford <matt@bitzesty.com>
2012-08-09 11:54:44 +0100

    rename application to match repository

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TariffWeb::Application.config.secret_token = '74c6f7889a662230804792dd19a2cc6a970f119fc9cef320c3012916380f2b4a0540ec31dc0b033d12e3ae174464f3e642bd45305010398ed0a86142da83bd15'
+TradeTariffFrontend::Application.config.secret_token = '74c6f7889a662230804792dd19a2cc6a970f119fc9cef320c3012916380f2b4a0540ec31dc0b033d12e3ae174464f3e642bd45305010398ed0a86142da83bd15'
