01d4fc26b9c366da8d8a55897fa0e97f0bf81cc4
Matthew Ford <matt@bitzesty.com>
2012-08-28 12:27:34 +0100

    make footnotes uniq by code

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -57,7 +57,7 @@ class Commodity
   end
 
   def footnotes
-    [import_measures.map(&:footnotes).select(&:present?) + export_measures.map(&:footnotes).select(&:present?)].flatten.uniq
+    [import_measures.map(&:footnotes).select(&:present?) + export_measures.map(&:footnotes).select(&:present?)].flatten.uniq {|f|f.code}
   end
 
   def third_country_duty_rate
