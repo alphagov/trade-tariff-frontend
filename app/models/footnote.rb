67368af4f2fff682290eb26169481bb6f8577c39
Saulius Grigaliunas <saulius@ninja.lt>
2012-10-05 12:18:33 +0300

    Footnote description should be formatted.

--- app/models/footnote.rb
+++ app/models/footnote.rb
@@ -1,9 +1,13 @@
 require 'api_entity'
+require 'formatter'
 
 class Footnote
   include ApiEntity
+  include Models::Formatter
 
   attr_accessor :code, :description
+
+  format :description, with: DescriptionFormatter, using: [:description]
 
   def id
     @id ||= SecureRandom.hex(16)
