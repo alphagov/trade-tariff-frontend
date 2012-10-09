53b88c225862577516d8e6d4255c25d2b9770c11
Saulius Grigaliunas <saulius@ninja.lt>
2012-10-09 10:10:07 +0300

    Refactor Search and fix specs.

--- app/models/search.rb
+++ app/models/search.rb
@@ -32,24 +32,41 @@ class Search
 
     def commodities=(commodity_data)
       @commodities = commodity_data.map { |cd| Commodity.new(cd) }
+
+      self.commodity_headings = @commodities
+    end
+
+    # Extract Headings from Commodity object as we need to build
+    # a tree for representation.
+    # Also, add commodity to heading's commodity array so that they could
+    # be listed in the tree.
+    def commodity_headings=(commodities)
       @commodity_headings ||= []
 
-      @commodities.each do |commodity|
-        existing_heading = (@headings + @commodity_headings).detect{|heading| heading == commodity.heading }
-
-        if existing_heading.present?
+      commodities.each do |commodity|
+        if existing_heading = find_heading(commodity.heading)
           existing_heading.add_commodity commodity
         else
-          new_heading = Heading.new(commodity.heading.attributes.merge(chapter: commodity.chapter.attributes,
-                                                                       section: commodity.section.attributes))
-          new_heading.add_commodity commodity
-          @commodity_headings << new_heading
+          @commodity_headings << build_heading_from(commodity).tap {|heading|
+            heading.add_commodity commodity
+          }
         end
       end
     end
 
     def resulting_headings
       (headings + commodity_headings).sort_by(&:goods_nomenclature_item_id)
+    end
+
+    private
+
+    def find_heading(heading_for_search)
+      (@headings + @commodity_headings).detect{|heading| heading == heading_for_search }
+    end
+
+    def build_heading_from(commodity)
+      Heading.new(commodity.heading.attributes.merge(chapter: commodity.chapter.attributes,
+                                                     section: commodity.section.attributes))
     end
   end
 
