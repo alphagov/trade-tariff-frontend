34b3fb0e6cccb645fd2ac8da6b3fd77a83c57b50
James Abley <james.abley@gmail.com>
2012-10-09 16:47:44 +0100

    Add git SHA1 to healthcheck

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -2,6 +2,6 @@ class HealthcheckController < ActionController::Base
   def check
     # Check API connectivity
     Section.all
-    render json: {}
+    render json: { git_sha1: CURRENT_RELEASE_SHA }
   end
 end
