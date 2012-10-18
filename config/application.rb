0c08453f456fe508e5ad3c6e75e8d88c7030e488
Matthew Ford <matt@bitzesty.com>
2012-10-18 15:01:14 +0000

    include the print stylesheet in the precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,11 @@ module TradeTariffFrontend
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = false #will move to mongo for API
+
+    config.assets.precompile += %w{
+      tariff-print.css
+    }
+
     config.assets.prefix = "tariff"
 
     # Enable the asset pipeline
