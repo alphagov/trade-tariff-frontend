98ed88605c88fb7fd3d46bbae9f35b9e09dcc7fe
Matthew Ford <matt@bitzesty.com>
2012-10-19 14:32:03 +0100

    fix the build

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -4,6 +4,6 @@ describe 'Home page' do
   it 'successfully renders the Tariff home page' do
     visit root_path
 
-    page.should have_content 'Use the tariff'
+    page.should have_content 'Use the trade tariff'
   end
 end
