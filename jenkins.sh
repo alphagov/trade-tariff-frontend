9e901a64c0b46dbcfbcf5d0f24b8c75fc7b4ee0e
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-31 12:00:11 +0000

    Jenkins should build out assets as part of a build
    Make sure that any SCSS changes don't break the app when deployed.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-RAILS_ENV=test bundle exec rake ci:setup:rspec spec
+RAILS_ENV=test bundle exec rake ci:setup:rspec spec assets:clean assets:precompile
 RESULT=$?
 exit $RESULT
