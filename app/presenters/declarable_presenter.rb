8a8a0d674b2e86b418697fed5f205481472f583d
Saulius Grigaliunas <saulius@ninja.lt>
2013-01-09 16:38:04 +0200

    Prepare for presenting measures for single country.
    Extract/move to method import & export measures on DeclarablePresenter
    so filtering by country could be done in these two methods.

--- app/presenters/declarable_presenter.rb
+++ app/presenters/declarable_presenter.rb
@@ -1,20 +1,29 @@
 class DeclarablePresenter
-  attr_reader :declarable
+  attr_reader :declarable, :country
 
   MEURSING_DUTY_EXPRESSION_IDS = %w[12 14 21 25 27 29]
   MEURSING_TOOL_LINK = "http://ec.europa.eu/taxation_customs/dds2/taric/measures.jsp?Lang=en&SimDate=%{date}&Taric=%{commodity_code}&LangDescr=en"
 
-  def initialize(declarable)
+  def initialize(declarable, country = nil) # TODO pass actual country in
     @declarable = declarable
+    @country = country
   end
 
   def to_s
     declarable.formatted_description
   end
 
+  def import_measures
+    declarable.import_measures
+  end
+
+  def export_measures
+    declarable.export_measures
+  end
+
   def display_meursing_table?
-    ((declarable.import_measures.map(&:measure_components).flatten.map(&:duty_expression_id) +
-      declarable.export_measures.map(&:measure_components).flatten.map(&:duty_expression_id)) &
+    ((import_measures.map(&:measure_components).flatten.map(&:duty_expression_id) +
+      export_measures.map(&:measure_components).flatten.map(&:duty_expression_id)) &
        MEURSING_DUTY_EXPRESSION_IDS).any?
   end
 
@@ -23,63 +32,54 @@ class DeclarablePresenter
   end
 
   def national_vat_import_measures
-    declarable.import_measures
-              .select{|measure| measure.national? && measure.vat? }
-              .present_with(MeasurePresenter)
+    import_measures.select{|measure| measure.national? && measure.vat? }
+                   .present_with(MeasurePresenter)
   end
 
   def import_measures_for_third_country
-    declarable.third_country_duty_measures
-              .sort_by(&:sort_key)
-              .present_with(MeasurePresenter)
+    third_country_duty_measures.sort_by(&:sort_key)
+                               .present_with(MeasurePresenter)
   end
 
   def import_measures_for_specific_countries
-    declarable.import_measures
-              .select(&:for_specific_countries)
-              .sort_by(&:specific_country_sort_key)
-              .flatten
-              .present_with(MeasurePresenter)
+    import_measures.select(&:for_specific_countries)
+                   .sort_by(&:specific_country_sort_key)
+                   .flatten
+                   .present_with(MeasurePresenter)
   end
 
   def has_import_measures_for_specific_countries?
-    declarable.import_measures
-              .select(&:for_specific_countries)
-              .any?
+    import_measures.select(&:for_specific_countries)
+                   .any?
   end
 
   def has_export_measures_for_third_countries?
-    declarable.export_measures
-              .select(&:third_country)
-              .any?
+    export_measures.select(&:third_country)
+                   .any?
   end
 
   def export_measures_for_third_country
-    declarable.export_measures
-              .select(&:third_country)
-              .sort_by(&:sort_key)
-              .present_with(MeasurePresenter)
+    export_measures.select(&:third_country)
+                   .sort_by(&:sort_key)
+                   .present_with(MeasurePresenter)
   end
 
   def has_vat_or_excise_third_country_export_measures?
-    declarable.export_measures
-              .select(&:third_country)
-              .select(&:vat?)
-              .any?
+    export_measures.select(&:third_country)
+                   .select(&:vat?)
+                   .any?
   end
 
   def has_export_measures_for_specific_countries?
-    declarable.export_measures
-              .select(&:for_specific_countries)
-              .any?
+    export_measures.select(&:for_specific_countries)
+                   .any?
   end
 
   def export_measures_for_specific_countries
-    declarable.export_measures
-              .select(&:for_specific_countries)
-              .sort_by(&:specific_country_sort_key)
-              .flatten
-              .present_with(MeasurePresenter)
+    export_measures.select(&:for_specific_countries)
+                   .sort_by(&:specific_country_sort_key)
+                   .flatten
+                   .present_with(MeasurePresenter)
   end
 
   private
