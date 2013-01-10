b1c286975e86757be43a9b66459c050ea85e2234
James Abley <james.abley@gmail.com>
2013-01-10 15:22:00 +0000

    Remove dependency on govuk_setenv
    We're probably not going to use this on the CI boxes, so we should just
    set the desired environment variables explicitly.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
+export GOVUK_APP_DOMAIN=test.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.gov.uk
+env
+
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-govuk_setenv tariff bundle exec rake RAILS_ENV=test ci:setup:rspec spec assets:clean assets:precompile
+bundle exec rake RAILS_ENV=test ci:setup:rspec spec assets:clean assets:precompile
 RESULT=$?
 exit $RESULT
