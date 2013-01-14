7483a2e877534ccb332a925cb3c39388acbc59e2
Saulius Grigaliunas <saulius@ninja.lt>
2013-01-14 11:08:14 +0200

    Remove stale specs.

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -1,40 +1,6 @@
 require 'spec_helper'
 
 describe Measure do
-  describe ".all" do
-    context "if looking for heading measures", :webmock do
-      let(:params) { { heading_id: Forgery(:basic).number } }
-
-      before {
-        stub_request(:get, "http://www.example.com/api/headings/#{params[:heading_id]}/import").
-               to_return(status: 200, body: '{"third_country_measures":[], "specific_measures": {}}',
-                         headers: { content_type: 'application/json' })
-      }
-
-      # it 'returns headings measures of requested type' do
-      #   Measure.all(params.merge(type: "import"))
-
-      #   a_request(:get, "www.example.com/api/headings/#{params[:heading_id]}/import").should have_been_made
-      # end
-    end
-
-    context "if looking for commodity measures", :webmock do
-      let(:params) { { commodity_id: Forgery(:basic).number } }
-
-      before {
-        stub_request(:get, "http://www.example.com/api/commodities/#{params[:commodity_id]}/export").
-               to_return(status: 200, body: '{"third_country_measures":[], "specific_measures": {}}',
-                         headers: { content_type: 'application/json' })
-      }
-
-      # it 'returns commodity measures of requested type' do
-      #   Measure.all(params.merge(type: "export"))
-
-      #   a_request(:get, "www.example.com/api/commodities/#{params[:commodity_id]}/export").should have_been_made
-      # end
-    end
-  end
-
   describe "#relevant_for_country?" do
     it 'returns true if geographical area code matches' do
       measure = Measure.new(attributes_for(:measure, geographical_area: {iso_code: 'br',
