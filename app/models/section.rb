44a8cf33f55e2b0be3e29aa262c5361cc75e7e6f
Saulius Grigaliunas <saulius@ninja.lt>
2013-07-02 14:09:05 +0300

    Use chapters_from, chapters_to to display section chapter range

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,20 +5,34 @@ class Section
 
   collection_path "/sections"
 
-  attr_accessor :numeral, :position, :title, :chapters, :section_note
+  attr_accessor :numeral,
+                :position,
+                :title,
+                :chapters,
+                :section_note,
+                :chapter_from,
+                :chapter_to
 
   has_many :chapters
 
+  def chapter_from=(chapter_from)
+    @chapter_from = chapter_from.to_i
+  end
+
+  def chapter_to=(chapter_to)
+    @chapter_to = chapter_to.to_i
+  end
+
+  def chapters_title
+    if chapter_from == chapter_to
+      "chapter #{chapter_from}"
+    else
+      "chapters #{chapter_from} to #{chapter_to}"
+    end
+  end
+
   def to_param
     position
-  end
-
-  def title_without_chapters
-    title.split(/\(.*\)/).first.strip
-  end
-
-  def title_chapters
-    title.match(/\(.*\)/)[0]
   end
 
   def to_s
