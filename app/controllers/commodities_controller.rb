71f3268b962d23eb09a06a921420b7959e03f361
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-28 15:39:20 +0200

    Remove unused code

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -5,19 +5,4 @@ class CommoditiesController < GoodsNomenclaturesController
     @chapter = @commodity.chapter
     @section = @commodity.section
   end
-
-  def changes
-    @commodity = CommodityPresenter.new(Commodity.find(params[:id], query_params))
-    @changes = ChangesPresenter.new(@commodity.changes)
-
-    respond_to do |format|
-      format.atom
-    end
-  end
-
-  private
-
-  def goods_code_id
-    super.first(10)
-  end
 end
--- app/controllers/headings_controller.rb
+++ app/controllers/headings_controller.rb
@@ -3,19 +3,4 @@ class HeadingsController < GoodsNomenclaturesController
     @heading = HeadingPresenter.new(Heading.find(params[:id], query_params))
     @commodities = HeadingCommodityPresenter.new(@heading.commodities)
   end
-
-  def changes
-    @heading = HeadingPresenter.new(Heading.find(params[:id], query_params))
-    @changes = ChangesPresenter.new(@heading.changes)
-
-    respond_to do |format|
-      format.atom
-    end
-  end
-
-  private
-
-  def goods_code_id
-    super.first(4)
-  end
 end
