263db114c0ab960f49ec48c0e034e9b7dd989251
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-29 10:12:46 +0200

    Ensure query for changes includes actual date
    Adds specs for cases when changes are found, not found or the entity
    (chapter, commodity, heading) itself is not found

--- spec/controllers/chapters/changes_controller_spec.rb
+++ spec/controllers/chapters/changes_controller_spec.rb
@@ -1,13 +1,37 @@
 require 'spec_helper'
 
-describe Chapters::ChangesController, "GET to #index", vcr: { cassette_name: "chapters_changes#index" }  do
+describe Chapters::ChangesController, "GET to #index" do
   let!(:chapter)   { Chapter.new(attributes_for :chapter, goods_nomenclature_item_id: "0100000000") }
 
-  before(:each) do
-    get :index, chapter_id: chapter.short_code, format: :atom
+  context 'chapter is valid at given date', vcr: { cassette_name: "chapters_changes#index" } do
+    before(:each) do
+      get :index, chapter_id: chapter.short_code, format: :atom
+    end
+
+    it { should respond_with(:success) }
+    it { should assign_to(:changeable) }
+    it { should assign_to(:changes) }
   end
 
-  it { should respond_with(:success) }
-  it { should assign_to(:changeable) }
-  it { should assign_to(:changes) }
+  context 'chapter has no changes at given date', vcr: { cassette_name: "chapters_changes#index_0100000000_1972-01-01" } do
+    before(:each) do
+      get :index, chapter_id: chapter.short_code, as_of: Date.new(1972,1,1), format: :atom
+    end
+
+    it { should respond_with(:success) }
+    it { should assign_to(:changeable) }
+    it { should assign_to(:changes) }
+
+    it 'fetches no changes' do
+      expect(assigns[:changes]).to be_empty
+    end
+  end
+
+  context 'chapter is not valid at given date', vcr: { cassette_name: "chapters_changes#index_0100000000_1970-01-01" } do
+    before(:each) do
+      get :index, chapter_id: chapter.short_code, as_of: Date.new(1970,1,1), format: :atom
+    end
+
+    it { should respond_with(:not_found) }
+  end
 end
