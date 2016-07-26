8503393c128927bcd3efe646a14cb68b61cd1cf9
Hildebrando Rueda <harq.soft@gmail.com>
2016-07-26 04:29:35 -0500

    The search_reference_presenter will not expect the referenced association (#20)

--- app/presenters/search_reference_presenter.rb
+++ app/presenters/search_reference_presenter.rb
@@ -1,24 +1,19 @@
 class SearchReferencePresenter
-  attr_reader :search_reference, :referenced_entity
-
-  delegate :link, to: :referenced_entity
+  attr_reader :search_reference
 
   def initialize(search_reference)
     @search_reference = search_reference
-    @referenced_entity = presenter_for(
-      search_reference.referenced_entity
-    ).new(search_reference.referenced_entity)
   end
 
   def to_s
     search_reference.title.titleize
   end
 
-  private
-
-  def presenter_for(referenced_entity)
-    "#{referenced_entity.class}Presenter".classify.constantize
+  def link
+    "/#{APP_SLUG}/#{referenced_class.tableize}/#{referenced_id}"
   end
+
+  private
 
   def method_missing(*args, &block)
     @search_reference.send(*args, &block)
