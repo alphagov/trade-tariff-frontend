1303dfcddb312407bda8ffddfcf01739e2d26eaa
Hildebrando Rueda <harq.soft@gmail.com>
2016-07-27 13:32:04 -0500

    Custom errors pages (#19)
    This project should be started from foreman starting all the apps
    
    * Add errors controller and views
    
    * Serve error pages from rails itself
    
    This way we can access the layout
    
    * Remove default html pages

--- config/routes.rb
+++ config/routes.rb
@@ -46,4 +46,7 @@ TradeTariffFrontend::Application.routes.draw do
   end
 
   root to: redirect("/#{APP_SLUG}", status: 302)
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
