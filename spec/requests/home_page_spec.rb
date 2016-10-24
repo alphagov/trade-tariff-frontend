861c85d698446031fd9282ccf859dd14a779e3e6
Matthew Ford <matt@bitzesty.com>
2016-10-24 16:01:13 +0100

    update homepage spec with copy change

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -5,7 +5,7 @@ describe 'Home page', type: :request do
     VCR.use_cassette('geographical_areas#countries') do
       visit root_path
 
-      expect(page).to have_content 'Use the Trade Tariff to search for commodity codes.'
+      expect(page).to have_content 'Trade Tariff: look up commodity codes, duty and VAT rates'
       expect(page).to have_selector('meta[name="description"][content="Search for import and export commodity codes and for tax, duty and licences that apply to your goods"]', visible: false)
     end
   end
