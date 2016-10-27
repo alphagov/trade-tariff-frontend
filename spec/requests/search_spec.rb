b488852f768b40c080c82dcb09d58714cf614902
Hildebrando Rueda <harq.soft@gmail.com>
2016-10-27 15:53:37 -0500

    Search now is made with a post instead of a get request

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -63,17 +63,22 @@ describe 'Search page', type: :request do
         end
       end
     end
-  end
 
-  context "duplicate results - when search results page is finished" do
-    before {
-      VCR.use_cassette("search#duplicate_results") do
-        visit perform_search_path(t: "synonym 1")
+    context "duplicate results - when search results page is finished" do
+      it "Display section when matching" do
+        VCR.use_cassette('tariff_updates#index') do
+          VCR.use_cassette('geographical_areas#countries') do
+            visit sections_path
+            VCR.use_cassette("search#duplicate_results") do
+              within("#new_search") do
+                fill_in "t", with: "synonym 1"
+                click_button "Search"
+              end
+              expect(page).to have_content("Section I")
+            end
+          end
+        end
       end
-    }
-
-    it {
-      expect(page).to have_content("Section I")
-    }
+    end
   end
 end
