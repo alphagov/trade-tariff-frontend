b7d4c471a0502c2fbf79f7909c6b9924e2fcdde0
Matthew Ford <matt@bitzesty.com>
2017-04-11 22:05:26 +0100

    update GA tracking code

--- app/assets/javascripts/analytics-init.js
+++ app/assets/javascripts/analytics-init.js
@@ -4,28 +4,14 @@
   // Load Google Analytics libraries
   GOVUK.Analytics.load();
 
-  // Use document.domain in dev, preview and staging so that tracking works
-  // Otherwise explicitly set the domain as www.gov.uk (and not gov.uk).
   var cookieDomain = (document.domain === 'www.gov.uk') ? '.www.gov.uk' : document.domain;
 
   // Configure profiles and make interface public
   // for custom dimensions, virtual pageviews and events
   GOVUK.analytics = new GOVUK.Analytics({
-    universalId: 'UA-26179049-1',
+    universalId: 'UA-97208357-1',
     cookieDomain: cookieDomain
   });
-
-  var $section = $('head meta[name="govuk:section"]');
-
-  // Set custom dimensions before tracking pageviews
-  if ($section) {
-    GOVUK.analytics.setDimension(1, $section.attr('content'));
-  }
-
-  GOVUK.analytics.setDimension(2, 'custom-tool'); // Format
-  if (window.devicePixelRatio) {
-    GOVUK.analytics.setDimension(11, window.devicePixelRatio);
-  }
 
   // Activate any event plugins eg. print intent, error tracking
   GOVUK.analyticsPlugins.error();
